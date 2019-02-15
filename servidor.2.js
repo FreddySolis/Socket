df00099c3ba15d4a3617d1210a23bc04f2502695
FreddySolis <173239@ids.upchiapas.edu.mx>
2019-02-15 09:52:06 -0600

    Segundo y tercer server

--- servidor.2.js
+++ servidor.2.js
@@ -6,21 +6,10 @@ const os = require('os')
 var interface = os.networkInterfaces()
 const server = require('http').Server(aplicacion) // Es lo que instalamos, y va a funcionar como servidor
 const socket = require('socket.io')(server)
-//for(var k in interface){
-  //  for(var k2 in interface[k]){
 
-    //    var address=interface[k][k2];
-      //  if(address.family=="IPv4", !address.internal){
-        //    ipdinamic =address.address.toString();
-
-          //  console.log(ipdinamic)
-
-        //}
-   // }
-//}
 
 var HOST = "alfredosolis173239.ddns.net"
-var PORT = "5000"
+var PORT = "5002"
 
 
 var ser = net.createServer(function(so){
